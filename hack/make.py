f92c4a7326aff805ab3bd3bef5c4e1f5adcf0d27
Tamal Saha <tamal@appscode.com>
2016-12-20 18:49:56 -0800

    Add check_antipackage

--- hack/make.py
+++ hack/make.py
@@ -1,5 +1,30 @@
 #!/usr/bin/env python
 
+
+# http://stackoverflow.com/a/14050282
+def check_antipackage():
+    from sys import version_info
+    sys_version = version_info[:2]
+    found = True
+    if sys_version < (3, 0):
+        # 'python 2'
+        from pkgutil import find_loader
+        found = find_loader('antipackage') is not None
+    elif sys_version <= (3, 3):
+        # 'python <= 3.3'
+        from importlib import find_loader
+        found = find_loader('antipackage') is not None
+    else:
+        # 'python >= 3.4'
+        from importlib import util
+        found = util.find_spec('antipackage') is not None
+    if not found:
+        print('Install missing package "antipackage"')
+        print('Example: pip install git+https://github.com/ellisonbg/antipackage.git#egg=antipackage')
+        from sys import exit
+        exit(1)
+check_antipackage()
+
 # ref: https://github.com/ellisonbg/antipackage
 import antipackage
 from github.appscode.libbuild import libbuild
@@ -46,7 +71,7 @@ def check_output(cmd, stdin=None, cwd=libbuild.REPO_ROOT):
 def version():
     # json.dump(BUILD_METADATA, sys.stdout, sort_keys=True, indent=2)
     for k in sorted(BUILD_METADATA):
-        print k + '=' + BUILD_METADATA[k]
+        print(k + '=' + BUILD_METADATA[k])
 
 
 def fmt():
@@ -61,42 +86,28 @@ def vet():
 def build_cmd(name):
     cfg = libbuild.BIN_MATRIX[name]
     if cfg['type'] == 'go':
-        if 'distro' in cfg.keys():
-            for goos, archs in cfg['distro'].iteritems():
+        if 'distro' in cfg:
+            for goos, archs in cfg['distro'].items():
                 for goarch in archs:
                     libbuild.go_build(name, goos, goarch, main='main.go')
         else:
             libbuild.go_build(name, libbuild.GOHOSTOS, libbuild.GOHOSTARCH, main='main.go')
 
 
-def build(name):
-    cfg = libbuild.BIN_MATRIX[name]
-    if cfg['type'] == 'go':
-        fmt()
-        build_cmd(name)
-
-
 def build_cmds():
     fmt()
-    for name in libbuild.BIN_MATRIX.keys():
+    for name in libbuild.BIN_MATRIX:
         build_cmd(name)
 
 
-def build_all():
-    build_cmds()
-
-
-def push_all():
-    dist = libbuild.REPO_ROOT + '/dist'
-    for name in os.listdir(dist):
-        d = dist + '/' + name
-        if os.path.isdir(d):
-            push_bin(d)
-
-
-def push(name):
-    bindir = libbuild.REPO_ROOT + '/dist/' + name
-    push_bin(bindir)
+def build(name=None):
+    if name:
+        cfg = libbuild.BIN_MATRIX[name]
+        if cfg['type'] == 'go':
+            fmt()
+            build_cmd(name)
+    else:
+        build_cmds()
 
 
 def push_bin(bindir):
@@ -105,6 +116,18 @@ def push_bin(bindir):
     for f in os.listdir(bindir):
         if os.path.isfile(bindir + '/' + f):
             libbuild.upload_to_cloud(bindir, f, BUILD_METADATA['version'])
+
+
+def push(name=None):
+    if name:
+        bindir = libbuild.REPO_ROOT + '/dist/' + name
+        push_bin(bindir)
+    else:
+        dist = libbuild.REPO_ROOT + '/dist'
+        for name in os.listdir(dist):
+            d = dist + '/' + name
+            if os.path.isdir(d):
+                push_bin(d)
 
 
 def update_registry():
