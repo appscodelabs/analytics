ffec4c8986509cee5940944c72a375dc785935e2
tamal <tamal@appscode.com>
2016-12-30 03:09:31 -0800

    Add SSL support

--- main.go
+++ main.go
@@ -1,15 +1,31 @@
 package main
 
 import (
+	"crypto/tls"
+	"crypto/x509"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net"
 	"net/http"
+	"time"
+
+	"github.com/appscode/go/flags"
+	flag "github.com/spf13/pflag"
 )
 
 // ref: http://stackoverflow.com/a/33301173
 func main() {
-	http.DefaultServeMux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	addr := flag.String("addr", ":60010", "host:port used by this server")
+	caCertFile := flag.String("caCertFile", "", "File containing CA certificate")
+	certFile := flag.String("certFile", "", "File container server TLS certificate")
+	keyFile := flag.String("keyFile", "", "File containing server TLS private key")
+
+	flags.InitFlags()
+	flags.DumpAll()
+
+	http.DefaultServeMux.HandleFunc("/index.json", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == "GET" {
 			ip, _, err := net.SplitHostPort(r.RemoteAddr)
 			if err != nil {
@@ -42,7 +58,49 @@ func main() {
 			return
 		}
 	})
-	if err := http.ListenAndServe(":5050", nil); err == nil {
-		fmt.Println(err)
+
+	log.Println("Listening on", *addr)
+
+	srv := &http.Server{
+		Addr:         *addr,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		Handler:      http.DefaultServeMux,
+	}
+	if *caCertFile == "" && *certFile == "" && *keyFile == "" {
+		log.Fatalln(srv.ListenAndServe())
+	} else {
+		/*
+			Ref:
+			 - https://blog.cloudflare.com/exposing-go-on-the-internet/
+			 - http://www.bite-code.com/2015/06/25/tls-mutual-auth-in-golang/
+			 - http://www.hydrogen18.com/blog/your-own-pki-tls-golang.html
+		*/
+		caCert, err := ioutil.ReadFile(*caCertFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		caCertPool := x509.NewCertPool()
+		caCertPool.AppendCertsFromPEM(caCert)
+
+		tlsConfig := &tls.Config{
+			PreferServerCipherSuites: true,
+			MinVersion:               tls.VersionTLS12,
+			SessionTicketsDisabled:   true,
+			CipherSuites: []uint16{
+				tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+				// tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305, // Go 1.8 only
+				// tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,   // Go 1.8 only
+				tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+				tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+			},
+			ClientCAs:  caCertPool,
+			ClientAuth: tls.VerifyClientCertIfGiven,
+		}
+		tlsConfig.BuildNameToCertificate()
+
+		srv.TLSConfig = tlsConfig
+		log.Fatalln(srv.ListenAndServeTLS(*certFile, *keyFile))
 	}
 }
