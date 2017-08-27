3656e28ab4f250764fce86a4ff97b7c6738fae5c
Abdullah Al Maruf - Tuhin <maruf@appscode.com>
2017-08-27 06:02:20 +0600

    Secrets added in deployment, some error return fixed (#12)

--- pkg/spreadsheet/spreadsheet.go
+++ pkg/spreadsheet/spreadsheet.go
@@ -8,7 +8,6 @@ import (
 	"net/http"
 	"net/url"
 	"os"
-	"os/user"
 	"path/filepath"
 
 	"github.com/appscode/errors"
@@ -18,51 +17,57 @@ import (
 	"google.golang.org/api/sheets/v4"
 )
 
+var Client_secret_path string
+
 // getClient uses a Context and Config to retrieve a Token
 // then generate a Client. It returns the generated Client.
-func GetClient(ctx context.Context, config *oauth2.Config) *http.Client {
+func GetClient(ctx context.Context, config *oauth2.Config) (*http.Client, error) {
 	cacheFile, err := tokenCacheFile()
 	if err != nil {
-		log.Fatalf("Unable to get path to cached credential file. %v", err)
+		return nil, errors.FromErr(err).Err()
 	}
 	tok, err := tokenFromFile(cacheFile)
 	if err != nil {
-		tok = getTokenFromWeb(config)
-		saveToken(cacheFile, tok)
+		log.Println(err)
+		// Browse the token and saves in file directory
+		tok, err = getTokenFromWeb(config)
+		if err != nil {
+			return nil, errors.FromErr(err).Err()
+		}
+		err = saveToken(cacheFile, tok)
+		if err != nil {
+			return nil, errors.FromErr(err).Err()
+		}
 	}
-	return config.Client(ctx, tok)
+	return config.Client(ctx, tok), nil
 }
 
 // getTokenFromWeb uses Config to request a Token.
 // It returns the retrieved Token.
-func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
+func getTokenFromWeb(config *oauth2.Config) (*oauth2.Token, error) {
 	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
 	fmt.Printf("Go to the following link in your browser then type the "+
 		"authorization code: \n%v\n", authURL)
 
 	var code string
 	if _, err := fmt.Scan(&code); err != nil {
-		log.Fatalf("Unable to read authorization code %v", err)
+		return nil, errors.FromErr(err).Err()
 	}
 
 	tok, err := config.Exchange(oauth2.NoContext, code)
 	if err != nil {
-		log.Fatalf("Unable to retrieve token from web %v", err)
+		return nil, errors.FromErr(err).Err()
 	}
-	return tok
+	return tok, nil
 }
 
 // tokenCacheFile generates credential file path/filename.
 // It returns the generated credential path/filename.
 func tokenCacheFile() (string, error) {
-	usr, err := user.Current()
-	if err != nil {
-		return "", err
-	}
-	tokenCacheDir := filepath.Join(usr.HomeDir, ".credentials")
+	tokenCacheDir := filepath.Join(Client_secret_path)
 	os.MkdirAll(tokenCacheDir, 0700)
 	return filepath.Join(tokenCacheDir,
-		url.QueryEscape("sheets.googleapis.com-go-api.json")), err
+		url.QueryEscape("sheet_access_token.json")), nil
 }
 
 // tokenFromFile retrieves a Token from a given file path.
@@ -80,21 +85,22 @@ func tokenFromFile(file string) (*oauth2.Token, error) {
 
 // saveToken uses a file path to create a file and store the
 // token in it.
-func saveToken(file string, token *oauth2.Token) {
+func saveToken(file string, token *oauth2.Token) error {
 	fmt.Printf("Saving credential file to: %s\n", file)
 	f, err := os.OpenFile(file, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
 	if err != nil {
-		log.Fatalf("Unable to cache oauth token: %v", err)
+		return errors.FromErr(err).Err()
 	}
 	defer f.Close()
 	json.NewEncoder(f).Encode(token)
+	return nil
 }
 
 func GetNewSheetService() (*sheets.Service, error) {
 	ctx := context.Background()
 	b, err := getClientSecret()
 	if err != nil {
-		return nil, errors.New("Unable to read client secret file").Err()
+		return nil, errors.FromErr(err).Err()
 	}
 	// If modifying these scopes, delete previously saved credentials
 	// at ~/.credentials/sheets.googleapis.com-go-api.json
@@ -102,18 +108,17 @@ func GetNewSheetService() (*sheets.Service, error) {
 	if err != nil {
 		return nil, errors.New("Unable to parse client secret file to config").Err()
 	}
-	client := GetClient(ctx, config)
+	client, err := GetClient(ctx, config)
+	if err != nil {
+		return nil, errors.FromErr(err).Err()
+	}
 	srv, err := sheets.New(client)
 	return srv, err
 }
 
-// File Path: ~/.credentials/client_secret_spreadsheet.json
+// File Path: /Client_Secret_Path
 func getClientSecret() ([]byte, error) {
-	usr, err := user.Current()
-	if err != nil {
-		return nil, err
-	}
-	fileDir := filepath.Join(usr.HomeDir, ".credentials")
+	fileDir := filepath.Join(Client_secret_path)
 	os.MkdirAll(fileDir, 0700)
 	return ioutil.ReadFile(filepath.Join(fileDir, url.QueryEscape("client_secret_spreadsheet.json")))
 }
